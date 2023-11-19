9a7378bf36d8d2eb7310dea0c8f8338eb732d52a
Pascal Malouin <pascal.malouin@gmail.com>
2023-11-19 18:29:28 -0500

    ...

--- setup.py
+++ setup.py
@@ -13,10 +13,11 @@ import os
 ## Custom command to download data during installation
 class CustomInstallCommand(install):
     def run(self):
+        print('hello')
         ael_directory_path = '/usr/share/ael'
         os.makedirs(ael_directory_path, exist_ok=True)
         # Add your logic to download the data from the Git repository
-        subprocess.check_call(['git', 'clone', 'https://github.com/alterEGO-Linux/ael-files.git', '/usr/share/ael/files'])
+        # subprocess.check_call(['git', 'clone', 'https://github.com/alterEGO-Linux/ael-files.git', '/usr/share/ael/files'])
         # Continue with the default installation process
         install.run(self)
 
